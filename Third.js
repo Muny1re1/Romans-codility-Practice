afd69f282199b5bf62d7e4865fe4b0413c040493
Muny1re1 <munyiriandy@gmail.com>
2024-07-24 12:43:22 +0300

    Third question done

--- Third.js
+++ Third.js
@@ -1,13 +1,18 @@
-// A word machine is a system that performs a sequence of simple operations on a stack of integers. Initially the stack is empty. The sequence of operations is given as a string. Operations are separated by single spaces. The following operations may be specified:
+// A word machine is a system that performs a sequence of simple operations on a stack of integers. Initially the stack is empty. 
+//The sequence of operations is given as a string. Operations are separated by single spaces. The following operations may be specified:
 
 // an integer X (from 0 to 220 − 1): the machine pushes X onto the stack;
 // "POP": the machine removes the topmost number from the stack;
 // "DUP": the machine pushes a duplicate of the topmost number onto the stack;
 // "+": the machine pops the two topmost elements from the stack, adds them together and pushes the sum onto the stack;
-// "-": the machine pops the two topmost elements from the stack, subtracts the second one from the first (topmost) one and pushes the difference onto the stack.
+// "-": the machine pops the two topmost elements from the stack, 
+//subtracts the second one from the first (topmost) one and pushes the difference onto the stack.
 // After processing all the operations, the machine returns the topmost value from the stack.
 
-// The machine processes 20-bit unsigned integers (numbers from 0 to 220 − 1). An overflow in addition or underflow in subtraction causes an error. The machine also reports an error when it tries to perform an operation that expects more numbers on the stack than the stack actually contains. Also, if, after performing all the operations, the stack is empty, the machine reports an error.
+// The machine processes 20-bit unsigned integers (numbers from 0 to 220 − 1).
+// An overflow in addition or underflow in subtraction causes an error. 
+//The machine also reports an error when it tries to perform an operation that expects more numbers on the stack than the stack actually contains.
+// Also, if, after performing all the operations, the stack is empty, the machine reports an error.
 
 // For example, given a string "4 5 6 - 7 +", the machine performs the following operations:
 
@@ -55,7 +60,8 @@
 //             |                     | 13, 7
 // Finally, the machine will return 7.
 
-// Given a string "5 6 + -", the machine reports an error. After the addition, there is only one number on the stack and the subtraction operation expects two.
+// Given a string "5 6 + -", the machine reports an error. 
+//After the addition, there is only one number on the stack and the subtraction operation expects two.
 
 // Given a string "3 DUP 5 - -", the machine reports an error. The second subtraction yields a negative result.
 
@@ -63,7 +69,9 @@
 
 // int solution(char *S);
 
-// that, given a string S containing a sequence of operations for the word machine, returns the result the machine would return after processing the operations. The function should return −1 if the machine would report an error while processing the operations.
+// that, given a string S containing a sequence of operations for the word machine, 
+//returns the result the machine would return after processing the operations. 
+//The function should return −1 if the machine would report an error while processing the operations.
 
 // Examples:
 
@@ -81,3 +89,78 @@
 
 // the length of string S is within the range [0..2,000];
 // S is a valid sequence of word machine operations.
+
+function algorithm(S){
+    // initializing the stack, operations, max value and result
+    let stack = [];
+    let operations = S.split(' ');
+    let MAX = 219
+    let result = 0;
+    console.log(operations);
+    
+    // iterating over each operation in the string and updating the result accordingly
+    for (let operation of operations){
+        if(operation === " "){
+            continue;
+        } else {
+            if(Number.isInteger(parseInt(operation))){
+                stack.push(parseInt(operation));
+            } else {
+                switch(operation){
+                    case "POP":
+                        if(stack.length < 1){
+                            result = -1
+                        }
+                        stack.pop();
+                        break;
+                    case "DUP":
+                        if(stack.length < 1){
+                            result = -1
+                        }
+                        stack.push(stack[stack.length - 1]);
+                        break;
+                    case "+":
+                        if(stack.length < 1){
+                            result = -1
+                        }
+                        let summation = stack.pop() + stack.pop();
+                        if(summation > MAX){
+                           result = -1
+                           console.log(result);
+                           return result
+                        }
+                        stack.push(summation);
+                        break;
+                    case "-":
+                        if(stack.length < 2) {
+                            result = -1
+                            console.log(result);
+                            return result;
+                        };
+                        let subtraction  = stack.pop() - stack.pop();
+                        if (subtraction < 0) {
+                            result = -1
+                            console.log(result);
+                            return result;
+                        };
+                        stack.push(subtraction);
+                        break;
+                    default:
+                        console.log(-1);
+                        return -1;
+                }
+                result = stack[stack.length - 1];
+            }
+        }
+    }
+    console.log(stack);
+    console.log(result);
+    return result;
+};
+
+// tests
+algorithm("4 5 6 - 7 + DUP");
+algorithm("13 DUP 4 POP 5 DUP + DUP + -");
+algorithm("5 6 + -");
+algorithm("3 DUP 5 - -");
+algorithm("1048575 DUP +");
